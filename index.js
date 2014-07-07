4302266123bc255591c2bb68a2a318b7a54514a3
mjsgreen01 <mjsgreen01@gmail.com>
2014-07-07 16:20:28 -0400

    Navbar functionality
    for desktop and mobile views,
    also eased scrolling added

--- index.js
+++ index.js
@@ -13,12 +13,90 @@ $(document).ready(function(){
 		if ($(window).scrollTop() > winHeight-75) {
 	        $('.mainNav').css('position', 'fixed');
 	        $('.mainNav').css('margin-top',-(winHeight-75));
+	        $('.homeSec2').css('margin-top','75px')
 	    }
 	    else{
 	    	$('.mainNav').css('position', 'static');
 	    	$('.mainNav').css('margin-top','0');
+	    	$('.homeSec2').css('margin-top','0px')
 	    }
 	});
+
+
+
+
+
+
+
+
+
+
+	//Navigation Bar Functionality
+	$("#workTab").click(function (e){
+        	e.preventDefault();
+            $('html, body').animate({
+                scrollTop: $(".homeSec2").offset().top -75
+            }, 1700, 'easeInOutCubic');
+    });
+	$("#aboutTab").click(function (e){
+        	e.preventDefault();
+            $('html, body').animate({
+                scrollTop: $(".homeSec3").offset().top -75
+            }, 1500, 'easeInOutCubic');
+    });
+    $(".name").click(function (e){
+        	e.preventDefault();
+            $('html, body').animate({
+                scrollTop: $(".homeSec1").offset().top
+            }, 1700, 'easeInOutCubic');
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    $(".navArrow").on({click:function(e){    
+	    	if($(this).hasClass('navArrowClicked')){
+	    		$(this).removeClass('navArrowClicked');
+	    		e.preventDefault();
+	            $('html, body').animate({
+	                scrollTop: $(".homeSec1").offset().top
+	            }, 1700, 'easeInOutCubic');	
+	    	}
+	    	else{
+	    		$(this).addClass('navArrowClicked');  
+	    		e.preventDefault();
+	            $('html, body').animate({
+	                scrollTop: $(".homeSec2").offset().top -75
+	            }, 1700, 'easeInOutCubic');	
+	    	}
+
+		}
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
