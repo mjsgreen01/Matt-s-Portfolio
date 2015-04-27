6e9999a8e83d7b483e458b279369c1a7a877bf33
mjsgreen01 <mjsgreen01@gmail.com>
2015-04-27 13:02:18 -0400

    more responsive design

--- mattgreenux/newHome1/index.js
+++ mattgreenux/newHome1/index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	var winHeight = $(window).height();
 
 	function homeResize () {
-		if ($(window).width()>560) {
+		if ($(window).width()>800) {
 			$('.previewContain').css('height',winHeight);
 		}
 	}
